0db693a74ff8ae2f6dfafdf39090419c68474bd8
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-02-09 18:08:55 +0100

    feat(course): poster youtube-player
    Mise en place d'un attribut poster & title sur le custom element youtube-player.

--- assets/js/elements/YoutubePlayer.js
+++ assets/js/elements/YoutubePlayer.js
@@ -18,14 +18,39 @@ export default class YoutubePlayer extends HTMLElement {
 
   constructor (attributes = {}) {
     super()
+
+    // Initialisation
     Object.keys(attributes).forEach((k) => this.setAttribute(k, attributes[k]))
     this.root = this.attachShadow({mode: 'open'})
     this.onYoutubePlayerStateChange = this.onYoutubePlayerStateChange.bind(this)
     this.onYoutubePlayerReady = this.onYoutubePlayerReady.bind(this)
+    this.getAttribute('poster')
+
+    // Structure HTML
+    let poster = this.getAttribute('poster')
+    poster = poster === null ? '' : `<div class="poster">
+      <img src="${poster}">
+      <svg class="play" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46 46"><path d="M23 0C10.32 0 0 10.32 0 23s10.32 23 23 23 23-10.32 23-23S35.68 0 23 0zm8.55 23.83l-12 8A1 1 0 0118 31V15a1 1 0 011.55-.83l12 8a1 1 0 010 1.66z"/></svg>
+      <div class="title">${this.getAttribute('title')}</div>
+    </div>`
     this.root.innerHTML = `
       ${this.buildStyles()}
-      <div><div class="player"></div></div>
-    `
+      <div class="ratio">
+        <div class="player"></div>
+        ${poster}
+      </div>`
+
+    // Evènements
+    if (poster !== '') {
+      let onClick = () => {
+        this.root.querySelector('.poster').setAttribute('aria-hidden', 'true')
+        this.setAttribute('autoplay', 'autoplay')
+        this.removeAttribute('poster')
+        this.loadPlayer(this.getAttribute('video'))
+        this.removeEventListener('click', onClick)
+      }
+      this.addEventListener('click', onClick)
+    }
   }
 
   disconnectedCallback () {
@@ -33,29 +58,37 @@ export default class YoutubePlayer extends HTMLElement {
   }
 
   async attributeChangedCallback (name, oldValue, newValue) {
-    if (name === 'video' && newValue !== null) {
-      await loadYoutubeApi()
-      if (this.player) {
-        this.player.cueVideoById(this.getAttribute('video'))
-        this.player.playVideo()
-        return
-      }
-      this.player = new YT.Player(this.root.querySelector('.player'), {
-        videoId: this.getAttribute('video'),
-        host: 'https://www.youtube-nocookie.com',
-        playerVars: {
-          autoplay: this.getAttribute('autoplay') ? 1 : 0,
-          loop: 0,
-          controls: 1,
-          showinfo: 0,
-          rel: 0
-        },
-        events: {
-          onStateChange: this.onYoutubePlayerStateChange,
-          onReady: this.onYoutubePlayerReady,
-        }
-      })
+    if (name === 'video' && newValue !== null && this.getAttribute('poster') === null) {
+      this.loadPlayer(newValue)
     }
+  }
+
+  /**
+   * @param {string} youtubeID
+   * @return {Promise<void>}
+   */
+  async loadPlayer (youtubeID) {
+    await loadYoutubeApi()
+    if (this.player) {
+      this.player.cueVideoById(this.getAttribute('video'))
+      this.player.playVideo()
+      return
+    }
+    this.player = new YT.Player(this.root.querySelector('.player'), {
+      videoId: youtubeID,
+      host: 'https://www.youtube-nocookie.com',
+      playerVars: {
+        autoplay: this.getAttribute('autoplay') ? 1 : 0,
+        loop: 0,
+        controls: 1,
+        showinfo: 0,
+        rel: 0
+      },
+      events: {
+        onStateChange: this.onYoutubePlayerStateChange,
+        onReady: this.onYoutubePlayerReady,
+      }
+    })
   }
 
   /**
@@ -85,12 +118,52 @@ export default class YoutubePlayer extends HTMLElement {
    */
   buildStyles () {
     return `<style>
-      div {
+      .ratio {
         background-color:black;
         position: relative;
         padding-bottom: 56.25%;
       }
-      iframe {
+      .poster {
+        cursor: pointer;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+      .poster:hover .play {
+        transform: scale(1.1)
+      }
+      .poster:hover::before {
+        opacity: .8;
+      }
+      .title {
+        color: #FFF;
+        font-size: 22px;
+        position: relative;
+        text-align: center;
+        z-index: 3;
+        transition: .3s;
+      }
+      .play {
+        position: relative;
+        width: 48px;
+        height: 48px;
+        border-radius: 48px;
+        z-index: 3;
+        fill: #FFF;
+        margin-bottom: 8px;
+        box-shadow:  0 1px 20px #121C4280;
+        transition: .3s;
+      }
+      .poster::before {
+        content:'';
+        background: linear-gradient(to top, var(--color) 0%, var(--color-transparent) 100%);
+        z-index: 2;
+      }
+      .poster,
+      iframe,
+      .poster::before,
+      img {
         position: absolute;
         top:0;
         left: 0;
@@ -98,6 +171,11 @@ export default class YoutubePlayer extends HTMLElement {
         right: 0;
         width: 100%;
         height: 100%;
+        transition: opacity .5s;
+      }
+      .poster[aria-hidden] {
+        pointer-events: none;
+        opacity: 0;
       }
     </style>`
   }
