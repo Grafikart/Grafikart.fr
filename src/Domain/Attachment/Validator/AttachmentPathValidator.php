cc46226d66990444a2b740917803cefd4d36703f
Twicode <github@twicode.com>
2020-02-19 22:10:09 +0100

    Update AttachmentPathValidator.php
    Pour ne pas démordre de Datetime, je pense que ça devrait pouvoir éviter n'importe quelles "sneaky values" ;)

--- src/Domain/Attachment/Validator/AttachmentPathValidator.php
+++ src/Domain/Attachment/Validator/AttachmentPathValidator.php
@@ -10,7 +10,9 @@ class AttachmentPathValidator
 
     static public function validate(string $value): bool
     {
-        return preg_match('/^2\d{3}\/(1[0-2]|0[1-9])$/', $value) > 0;
+        $format = 'Y/m';
+        $datetime = DateTime::createFromFormat($format, $value);
+        return $datetime->format($format) === $value;
     }
 
 }
