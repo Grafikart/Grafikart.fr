feb591e5b10768b83c8604aa27f65447a8682990
Jonthan Boyer <contact@grafikart.fr>
2020-03-11 09:22:38 +0100

    fix(test): initialisation base de données

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\NullOutput;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
@@ -13,8 +14,8 @@ $kernel = new \App\Kernel('test', true);
 $kernel->boot();
 $application = new Application($kernel);
 $application->setAutoExit(false);
-$user = $application->run(new StringInput('doctrine:query:sql "SELECT 1 FROM user;" -q'));
-if ($user !== 0) {
+$databaseDoesNotExists = $application->run(new StringInput('doctrine:query:sql "SELECT username FROM user;"'), new NullOutput());
+if ($databaseDoesNotExists) {
     $application->run(new StringInput('doctrine:database:drop --if-exists --force -q'));
     $application->run(new StringInput('doctrine:database:create -q'));
     $application->run(new StringInput('doctrine:schema:update --force -q'));
