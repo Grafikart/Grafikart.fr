bfd0b433adebed0da3a4992c422b29bbfc8127f4
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-05-08 18:24:16 +0200

    test: API POST /notifications

---
+++ tests/Http/Api/NotificationApiTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Tests\Http\Api;
+
+use App\Tests\ApiTestCase;
+use App\Tests\FixturesTrait;
+use App\Tests\Infrastructure\Mercure\MercureAssertions;
+use Symfony\Component\HttpFoundation\Response;
+
+class NotificationApiTest extends ApiTestCase
+{
+    use FixturesTrait;
+    use MercureAssertions;
+
+    public function notificationData(): iterable
+    {
+        yield [
+            [
+                'message' => 'Hello world',
+                'channel' => 'global',
+                'url' => 'https://grafikart.fr/grafikart/live'
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider notificationData
+     */
+    public function testCreateUnauthenticated(array $data): void
+    {
+        $this->client->request('POST', '/api/notifications', ['json' => $data]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @dataProvider notificationData
+     */
+    public function testCreateBadRequest(array $data): void
+    {
+        $data['url'] = 'azeazeeazaz';
+        $users = $this->loadFixtures(['users']);
+        $this->login($users['user_admin']);
+        $this->client->request('POST', '/api/notifications', [
+            'json' => $data
+        ]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @dataProvider notificationData
+     */
+    public function testCreateOk(array $data): void
+    {
+        $users = $this->loadFixtures(['users']);
+        $this->login($users['user_admin']);
+        $this->client->request('POST', '/api/notifications', [
+            'json' => $data
+        ]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+    }
+
+    /**
+     * @dataProvider notificationData
+     */
+    public function testCreatePublishOnMercure(array $data): void
+    {
+        $users = $this->loadFixtures(['users']);
+        $this->login($users['user_admin']);
+        $this->client->request('POST', '/api/notifications', [
+            'json' => $data
+        ]);
+        $this->assertPublishedOnTopic('/notifications/global');
+    }
+
+}
---
+++ tests/Infrastructure/Mercure/MercureAssertions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Infrastructure\Mercure;
+
+use Symfony\Component\Mercure\Update;
+
+trait MercureAssertions
+{
+
+    /**
+     * Vérifie qu'une publication a eu lieu sur Mercure
+     *
+     * @param string $topic
+     */
+    public function assertPublishedOnTopic(string $topic): void
+    {
+        $this->assertNotNull(PublisherStub::$lastUpdate);
+        /** @var Update $update */
+        $update = PublisherStub::$lastUpdate;
+        $this->assertContains($topic, $update->getTopics());
+    }
+
+}
