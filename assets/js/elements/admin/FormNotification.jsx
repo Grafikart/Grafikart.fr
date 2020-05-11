1ac87d02cc731ab7aaed8b53873d43a9e893244b
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-05-11 14:41:15 +0200

    fix: nettoyage des dépendances
    Suppression de react-spring / robot3 (utilisé pour tester);
    Nettoyage des composants preact du forum.

--- assets/js/elements/admin/FormNotification.jsx
+++ assets/js/elements/admin/FormNotification.jsx
@@ -1,21 +1,19 @@
 import preactCustomElement from '@fn/preact'
 import {FetchForm, FormField, FormPrimaryButton} from '@comp/Form'
 import {useState} from 'preact/hooks'
-import {FloatingAlert} from '@comp/Alert'
+import {flash} from '@el/Alert'
 
 function FormNotification () {
 
   const initialData = {message: '', url: window.location.origin}
   const [data, setData] = useState(initialData)
-  const [success, setSuccess] = useState(false)
 
   const onSuccess = function () {
-    setSuccess(true)
+    flash('Notification envoyée avec succès')
     setData(initialData)
   }
 
   return <FetchForm action="/api/notifications" value={data} className="stack" onChange={setData} onSuccess={setSuccess}>
-    {success && <FloatingAlert type="success">Notification envoyée avec succès</FloatingAlert>}
     <FormField name="message" type="textarea"/>
     <FormField name="url"/>
     <FormPrimaryButton>Notifier</FormPrimaryButton>
