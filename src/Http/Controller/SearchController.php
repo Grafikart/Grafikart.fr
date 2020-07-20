2ce2632d7b1d4b6de4b59b818107f95c28fb86ca
blouislhomme <benjamin.louis-lhomme@orange.fr>
2020-07-20 14:16:35 +0200

    Recherche : redirection vers une techno qui matche

--- src/Http/Controller/SearchController.php
+++ src/Http/Controller/SearchController.php
@@ -7,18 +7,50 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Domain\Course\Entity\Technology;
+use App\Http\Normalizer\TechnologyPathNormalizer;
+
+
 class SearchController extends AbstractController
 {
     /**
      * @Route("/search", name="search")
      */
-    public function search(Request $request, SearchInterface $search): Response
-    {
+    public function search(
+      Request $request,
+      SearchInterface $search,
+      TechnologyPathNormalizer $technologyPathNormalizer
+    ): Response {
         $q = $request->query->get('q');
 
-        return $this->render('pages/search.html.twig', [
-            'q' => $q,
-            'results' => $search->search($q, [])['hits'],
-        ]);
+        // Si q n'est pas vide
+        if ( !empty( $q ) ) {
+
+          // on commence par chercher une techno correspondante
+          $technology = $this->getDoctrine()
+              ->getRepository(Technology::class)
+              ->findOneBy( ['name' => $q] );
+
+          // si ça matche on fait la redirection vers la techno en normalisant le path
+          if ( !empty( $technology ) ) {
+            return $this->redirectToRoute(
+              $technologyPathNormalizer->normalize( $technology )['path'],
+              $technologyPathNormalizer->normalize( $technology )['params']
+            );
+          }
+
+          // si ça ne matche pas on fait la recherche typesense
+          else {
+            return $this->render('pages/search.html.twig', [
+                'q' => $q,
+                'results' => $search->search($q, [])['hits'],
+            ]);
+          }
+        }
+
+        // et enfin si q est vide on redirige vers une page d'erreur
+        else {
+          return $this->redirectToRoute( 'path_page_derreur' );
+        }
     }
 }
