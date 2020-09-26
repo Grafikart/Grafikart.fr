916cabcc42ccc0b89e1cd3d003f3db9635d3c86e
Jonthan Boyer <contact@grafikart.fr>
2020-09-26 21:31:27 +0200

    feat: lazy pdo connection

--- src/Infrastructure/Importer/MySQLImporter.php
+++ src/Infrastructure/Importer/MySQLImporter.php
@@ -14,8 +14,6 @@ abstract class MySQLImporter extends DataImporter
     public function __construct(\PDO $pdo, EntityManagerInterface $em, KernelInterface $kernel)
     {
         $this->pdo = $pdo;
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         parent::__construct($em, $kernel);
     }
 }
--- src/Infrastructure/Importer/UserImporter.php
+++ src/Infrastructure/Importer/UserImporter.php
@@ -16,8 +16,6 @@ final class UserImporter implements TypeImporterInterface
     public function __construct(\PDO $pdo, EntityManagerInterface $em)
     {
         $this->pdo = $pdo;
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         $this->em = $em;
     }
 
