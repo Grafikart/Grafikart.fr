8bcf75224f7e9a188fd898bbddd3d45d9927a3a7
oussaka <oussaka@users.noreply.github.com>
2020-10-07 23:48:28 +0200

    fix(notification) send notifcation to all users who replied to the topic

--- src/Domain/Notification/Subscriber/ForumMessageSubscriber.php
+++ src/Domain/Notification/Subscriber/ForumMessageSubscriber.php
@@ -3,16 +3,19 @@
 namespace App\Domain\Notification\Subscriber;
 
 use App\Domain\Forum\Event\MessageCreatedEvent;
+use App\Domain\Forum\TopicService;
 use App\Domain\Notification\NotificationService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class ForumMessageSubscriber implements EventSubscriberInterface
 {
     private NotificationService $service;
+    private TopicService $topicService;
 
-    public function __construct(NotificationService $service)
+    public function __construct(NotificationService $service, TopicService $topicService)
     {
         $this->service = $service;
+        $this->topicService = $topicService;
     }
 
     public static function getSubscribedEvents()
@@ -27,7 +30,12 @@ class ForumMessageSubscriber implements EventSubscriberInterface
         $message = $event->getMessage();
         $name = $message->getAuthor()->getUsername();
         $topic = $message->getTopic();
-        $user = $topic->getAuthor();
-        $this->service->notifyUser($user, "**$name** a répondu à votre sujet {$topic->getName()}", $message);
+
+        // On récupère les utilisateurs à notifier
+        $users = $this->topicService->usersToNotify($event->getMessage());
+
+        foreach ($users as $user) {
+            $this->service->notifyUser($user, "**$name** a répondu à votre sujet {$topic->getName()}", $message);
+        }
     }
 }
