72d621c69311c290cc3470fedddd54b42a67327a
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-10-09 17:58:09 +0200

    feat(maker): générateur de subscriber

--- src/Infrastructure/Maker/AbstractMakeCommand.php
+++ src/Infrastructure/Maker/AbstractMakeCommand.php
@@ -3,6 +3,12 @@
 namespace App\Infrastructure\Maker;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 use Twig\Environment;
 
 abstract class AbstractMakeCommand extends Command
@@ -26,5 +32,36 @@ abstract class AbstractMakeCommand extends Command
             mkdir($directory, 0777, true);
         }
         file_put_contents($filename, $content);
+    }
+
+    /**
+     * Demande à l'utilisateur de choisir une class parmis une liste correspondant au motif
+     */
+    protected function askClasses (string $question, string $pattern, SymfonyStyle $io): array
+    {
+        // On construit la liste utilisé pour l'autocompletion
+        $classes = [];
+        $files = (new Finder())->in("{$this->projectDir}/src")->name("*Event.php")->files();
+        /** @var SplFileInfo $file */
+        foreach($files as $file) {
+            $filename = str_replace('.php', '', $file->getBasename());
+            $namespace = 'App\\' . str_replace('/', '\\', $file->getRelativePath()) . '\\' . $filename;
+            $classes[$filename] = $namespace;
+        }
+
+        // On pose à l'utilisateur la question
+        $q = new Question($question);
+        $q->setAutocompleterValues(array_keys($classes));
+        $answers = [];
+        while(true) {
+            $class = $io->askQuestion($q);
+            if ($class === null) {
+                return $answers;
+            }
+            $answers[] = [
+                'namespace' => $classes[$class],
+                'class_name' => $class,
+            ];
+        }
     }
 }
---
+++ src/Infrastructure/Maker/MakeSubscriberCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Infrastructure\Maker;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class MakeSubscriberCommand extends AbstractMakeCommand
+{
+    protected static $defaultName = 'do:subscriber';
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Crée un EventSubscriber et le test associé')
+            ->addArgument('subscriberName', InputArgument::OPTIONAL, "Nom de l'EventSubscriber")
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $events = $this->askClasses("Quel évènement écouter ?", "*Event", $io);
+
+        /** @var string $subscriberPath */
+        $subscriberPath = $input->getArgument('subscriberName');
+        if (substr($subscriberPath, -1 * strlen('Subscriber')) !== 'Subscriber') {
+            $subscriberPath .= 'Subscriber';
+        }
+        if (!is_string($subscriberPath)) {
+            throw new \RuntimeException('subscriberPath doit être une chaine de caractère');
+        }
+        $parts = explode('/', $subscriberPath);
+        $namespace = '\\'.implode('\\', array_slice($parts, 0, -1));
+        $className = $parts[count($parts) - 1];
+        $basePath = "src/";
+        $params = [
+            'events'    => $events,
+            'namespace' => $namespace,
+            'class_name' => $className
+        ];
+
+        $this->createFile('eventSubscriber', $params, "{$basePath}{$subscriberPath}.php");
+        $this->createFile('eventSubscriber.test', $params, str_replace("src/", "tests/", $basePath) . "{$subscriberPath}Test.php");
+
+        $io->success('Le subscriber a bien été créé');
+
+        return Command::SUCCESS;
+    }
+}
