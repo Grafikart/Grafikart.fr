a8f46afa7ff4d3d9c05220eaad92a4da4723e43f
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-10-28 22:09:21 +0100

    fix: couleur confetti

--- assets/elements/Confetti.js
+++ assets/elements/Confetti.js
@@ -1,15 +1,17 @@
-import confetti from 'canvas-confetti'
-import { windowHeight } from '/functions/window.js'
+import confetti from "canvas-confetti";
+import { windowHeight } from "/functions/window.js";
 
 export class Confetti extends HTMLElement {
-  connectedCallback () {
-    const rect = this.getBoundingClientRect()
-    const y = (rect.top + rect.height / 2) / windowHeight()
+  connectedCallback() {
+    const rect = this.getBoundingClientRect();
+    const y = (rect.top + rect.height / 2) / windowHeight();
     confetti({
       particleCount: 100,
       zIndex: 3000,
       spread: 70,
-      origin: { y }
-    })
+      disableForReducedMotion: true,
+      colors: ["#121c42", "#effbec", "#4869ee", "#f25353", "#41cf7c", "#feb32b"],
+      origin: { y },
+    });
   }
 }
