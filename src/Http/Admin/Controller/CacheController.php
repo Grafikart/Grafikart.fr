630dc8d571a57df7387c6dcd1dabd7f6f3fe8082
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-11-05 20:01:26 +0100

    feat: nettoyage cache

---
+++ src/Http/Admin/Controller/CacheController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Admin\Controller;
+
+use App\Http\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CacheController extends AbstractController
+{
+
+    /**
+     * @Route("/cache/clean", name="cache_clean", methods={"POST"})
+     */
+    public function clean(AdapterInterface $cache): RedirectResponse
+    {
+        $this->addFlash('success', 'Le cache a bien été supprimé');
+        $cache->clear();
+        return $this->redirectToRoute('admin_home');
+    }
+}
