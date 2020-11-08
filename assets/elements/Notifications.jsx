844185d616d403ea093d0a59d55970c4cc364748
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-11-08 19:14:41 +0100

    fix(style): responsive

--- assets/elements/Notifications.jsx
+++ assets/elements/Notifications.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useRef, useState } from 'preact/hooks'
-import { useAsyncEffect, useClickOutside, useNotificationCount, usePrepend } from '/functions/hooks.js'
-import { Icon } from '/components/Icon.jsx'
-import { SlideIn } from '/components/Animation/SlideIn.jsx'
-import { isAuthenticated, lastNotificationRead } from '/functions/auth.js'
-import { Spinner } from '/components/Animation/Spinner.jsx'
-import { loadNotifications, onNotification } from '/api/notifications.js'
-import { jsonFetch } from '/functions/api.js'
+import { useEffect, useRef, useState } from "preact/hooks";
+import { useAsyncEffect, useClickOutside, useNotificationCount, usePrepend } from "/functions/hooks.js";
+import { Icon } from "/components/Icon.jsx";
+import { SlideIn } from "/components/Animation/SlideIn.jsx";
+import { isAuthenticated, lastNotificationRead } from "/functions/auth.js";
+import { Spinner } from "/components/Animation/Spinner.jsx";
+import { loadNotifications, onNotification } from "/api/notifications.js";
+import { jsonFetch } from "/functions/api.js";
 
-const OPEN = 0
-const CLOSE = 1
+const OPEN = 0;
+const CLOSE = 1;
 
-function countUnread (notifications, notificationReadAt) {
+function countUnread(notifications, notificationReadAt) {
   return notifications.filter(({ createdAt }) => {
-    return notificationReadAt < Date.parse(createdAt)
-  }).length
+    return notificationReadAt < Date.parse(createdAt);
+  }).length;
 }
 
 /**
@@ -22,57 +22,56 @@ function countUnread (notifications, notificationReadAt) {
  * @return {*}
  * @constructor
  */
-export function Notifications () {
+export function Notifications() {
   // Hooks
-  const [state, setState] = useState(CLOSE)
-  const [notifications, pushNotification] = usePrepend()
-  const [notificationReadAt, setNotificationReadAt] = useState(lastNotificationRead())
-  const [loading, setLoading] = useState(true)
-  const unreadCount = countUnread(notifications, notificationReadAt)
-  useNotificationCount(unreadCount)
+  const [state, setState] = useState(CLOSE);
+  const [notifications, pushNotification] = usePrepend();
+  const [notificationReadAt, setNotificationReadAt] = useState(lastNotificationRead());
+  const [loading, setLoading] = useState(true);
+  const unreadCount = countUnread(notifications, notificationReadAt);
+  useNotificationCount(unreadCount);
 
   // Méthodes
-  const openMenu = e => {
-    e.preventDefault()
-    setState(OPEN)
+  const openMenu = (e) => {
+    e.preventDefault();
+    setState(OPEN);
     if (unreadCount > 0) {
-      setNotificationReadAt(new Date())
-      jsonFetch('/api/notifications/read', { method: 'post' }).catch(console.error)
+      setNotificationReadAt(new Date());
+      jsonFetch("/api/notifications/read", { method: "post" }).catch(console.error);
     }
-  }
+  };
   const closeMenu = () => {
-    setState(CLOSE)
-  }
+    setState(CLOSE);
+  };
 
   // On charge les notification la première fois
   useAsyncEffect(async () => {
     if (isAuthenticated()) {
-      await loadNotifications()
-      setLoading(false)
+      await loadNotifications();
+      setLoading(false);
     }
-  }, [])
+  }, []);
 
   // On écoute l'arrivé de nouveaux évènement depuis l'API ou le SSE
-  useEffect(() => onNotification('notification', pushNotification), [pushNotification])
+  useEffect(() => onNotification("notification", pushNotification), [pushNotification]);
 
   // On écoute quand les messages sont marqués comme lu
   useEffect(() => {
-    return onNotification('markAsRead', () => {
-      console.log('azeazeea')
-      setNotificationReadAt(new Date())
-    })
-  }, [])
+    return onNotification("markAsRead", () => {
+      setNotificationReadAt(new Date());
+    });
+  }, []);
 
   // Le système de notification ne fonction que pour les utilisateurs
-  if (!isAuthenticated()) return null
+  if (!isAuthenticated()) return null;
 
   return (
     <>
       <button onClick={openMenu}>
-        <Icon name='bell' />
+        <Icon name="bell" />
       </button>
       <Badge count={unreadCount} />
-      <SlideIn className='notifications' show={state === OPEN}>
+      <SlideIn className="notifications" show={state === OPEN}>
         <Popup
           loading={loading}
           onClickOutside={closeMenu}
@@ -81,54 +80,59 @@ export function Notifications () {
         />
       </SlideIn>
     </>
-  )
+  );
 }
 
 /**
  * Badge contenant le nombre de notifications
  */
-function Badge ({ count }) {
-  return count > 0 && <span className='notification-badge'>{count}</span>
+function Badge({ count }) {
+  return count > 0 && <span className="notification-badge">{count}</span>;
 }
 
 /**
  * Popup contenant les notifications
  */
-function Popup ({ notifications = [], onClickOutside = () => {}, loading = false, notificationReadAt, ...props }) {
-  const ref = useRef()
+function Popup({ notifications = [], onClickOutside = () => {}, loading = false, notificationReadAt, ...props }) {
+  const ref = useRef();
 
-  useClickOutside(ref, onClickOutside)
+  useClickOutside(ref, onClickOutside);
 
   return (
     <div ref={ref} {...props}>
-      <div className='notifications_title'>Nouveaux messages</div>
-      <div className='notifications_body'>
+      <div className="notifications_title">
+        Nouveaux messages
+        <button aria-label="Fermer" onClick={onClickOutside}>
+          <Icon name="cross" />
+        </button>
+      </div>
+      <div className="notifications_body">
         {loading && <Spinner />}
-        {notifications.map(n => (
+        {notifications.map((n) => (
           <Notification key={n.id} notificationReadAt={notificationReadAt} {...n} />
         ))}
       </div>
-      <a href='/notifications' className='notifications_footer'>
+      <a href="/notifications" className="notifications_footer">
         Toutes les notifications
       </a>
     </div>
-  )
+  );
 }
 
 /**
  * Représente une notification
  */
-function Notification ({ url, message, createdAt, notificationReadAt }) {
-  const isRead = notificationReadAt > createdAt
-  const className = `notifications_item ${isRead ? 'is-read' : ''}`
-  const time = Date.parse(createdAt) / 1000
+function Notification({ url, message, createdAt, notificationReadAt }) {
+  const isRead = notificationReadAt > createdAt;
+  const className = `notifications_item ${isRead ? "is-read" : ""}`;
+  const time = Date.parse(createdAt) / 1000;
   // eslint-disable-next-line react/no-danger
   return (
     <a href={url} className={className}>
       <div dangerouslySetInnerHTML={{ __html: message }} />
-      <small class='text-muted'>
+      <small class="text-muted">
         <time-ago time={time} />
       </small>
     </a>
-  )
+  );
 }
