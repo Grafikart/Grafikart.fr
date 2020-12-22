f78ada3a8b3e2e773fb5a3c01e3c270bb81fc978
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-12-22 19:22:49 +0100

    style: lint

--- src/Http/Api/Controller/DiscordController.php
+++ src/Http/Api/Controller/DiscordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Api\Controller;
 
 use App\Domain\Auth\UserRepository;
 use App\Http\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DiscordController extends AbstractController
@@ -12,9 +13,9 @@ class DiscordController extends AbstractController
     /**
      * @Route("/discord/premium", name="country")
      */
-    public function premium (UserRepository $userRepository) {
+    public function premium(UserRepository $userRepository): JsonResponse
+    {
         $users = $userRepository->findPremiumDiscordIds();
         return $this->json($users);
     }
-
 }
