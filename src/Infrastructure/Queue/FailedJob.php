f1d6100d21567202cd003e4defb7ecbb156af6ad
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-09 00:55:05 +0100

    fix: depreciation stamp messenger

--- src/Infrastructure/Queue/FailedJob.php
+++ src/Infrastructure/Queue/FailedJob.php
@@ -4,22 +4,22 @@ namespace App\Infrastructure\Queue;
 
 use Symfony\Component\Messenger\Bridge\Doctrine\Transport\DoctrineReceivedStamp;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Stamp\ErrorDetailsStamp;
 use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
 
 class FailedJob
 {
-    private RedeliveryStamp $lastStamp;
     private Envelope $envelope;
     private string $id;
 
     public function __construct(Envelope $envelope)
     {
-        $this->lastStamp = $this->getLastRedeliveryStampWithException($envelope);
         $this->envelope = $envelope;
 
         // On obtient l'ID depuis le stamp doctrine
-        /** @var DoctrineReceivedStamp $stamp */
-        foreach (array_reverse($envelope->all(DoctrineReceivedStamp::class)) as $stamp) {
+        /** @var ?DoctrineReceivedStamp $stamp */
+        $stamp = $envelope->last(DoctrineReceivedStamp::class);
+        if ($stamp) {
             $this->id = $stamp->getId();
         }
     }
@@ -46,29 +46,29 @@ class FailedJob
 
     public function getErrorMessage(): string
     {
-        return $this->lastStamp->getExceptionMessage() ?: '';
+        /** @var ?ErrorDetailsStamp $stamp */
+        $stamp = $this->envelope->last(ErrorDetailsStamp::class);
+
+        return $stamp ? $stamp->getExceptionMessage() : '';
     }
 
     public function getFailedAt(): \DateTimeInterface
     {
-        return $this->lastStamp->getRedeliveredAt();
+        /** @var ?RedeliveryStamp $stamp */
+        $stamp = $this->envelope->last(RedeliveryStamp::class);
+
+        return $stamp ? $stamp->getRedeliveredAt() : new \DateTimeImmutable();
     }
 
     public function getTrace(): string
     {
-        $exception = $this->lastStamp->getFlattenException();
+        /** @var ?ErrorDetailsStamp $stamp */
+        $stamp = $this->envelope->last(ErrorDetailsStamp::class);
+        if (!$stamp) {
+            return '';
+        }
+        $exception = $stamp->getFlattenException();
 
         return $exception ? $exception->getTraceAsString() : '';
-    }
-
-    private function getLastRedeliveryStampWithException(Envelope $envelope): RedeliveryStamp
-    {
-        /** @var RedeliveryStamp $stamp */
-        foreach (array_reverse($envelope->all(RedeliveryStamp::class)) as $stamp) {
-            if (null !== $stamp->getExceptionMessage()) {
-                return $stamp;
-            }
-        }
-        throw new \RuntimeException('No exception for the job');
     }
 }
