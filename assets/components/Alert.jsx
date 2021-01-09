44586998f132ad817a66a922b79c7756da71d26e
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-09 10:13:41 +0100

    fix: ne plus masquer alert erreur

--- assets/components/Alert.jsx
+++ assets/components/Alert.jsx
@@ -1,6 +1,6 @@
 export function Alert ({ type = 'success', children, duration }) {
   return (
-    <alert-message type={type} className='full' duration={duration}>
+    <alert-message type={type} className='full' duration={type === 'success' ? duration : undefined}>
       {children}
     </alert-message>
   )
