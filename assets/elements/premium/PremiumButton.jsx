c32e83121403592a4687d3ed63131a1190ae0b2f
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-09 10:36:14 +0100

    fix: utilisation des clefs de paiements

--- assets/elements/premium/PremiumButton.jsx
+++ assets/elements/premium/PremiumButton.jsx
@@ -1,112 +1,118 @@
-import { Button, PrimaryButton } from "/components/Button.jsx";
-import { useToggle } from "/functions/hooks.js";
-import { SlideIn } from "/components/Animation/SlideIn.jsx";
-import { useEffect, useRef, useState } from "preact/hooks";
-import { classNames } from "/functions/dom.js";
-import { vatPrice } from "/functions/vat.js";
-import { ApiError, jsonFetch } from "/functions/api.js";
-import { redirect } from "/functions/url.js";
-import { flash } from "/elements/Alert.js";
-import scriptjs from "scriptjs";
-import { Stack } from "/components/Layout.jsx";
-import { Checkbox, Field } from "/components/Form.jsx";
-import { CountrySelect } from "/components/CountrySelect.jsx";
-import { importScript } from "/functions/script.js";
-import { isAuthenticated } from "/functions/auth.js";
+import { Button, PrimaryButton } from '/components/Button.jsx'
+import { useToggle } from '/functions/hooks.js'
+import { SlideIn } from '/components/Animation/SlideIn.jsx'
+import { useEffect, useRef, useState } from 'preact/hooks'
+import { classNames } from '/functions/dom.js'
+import { vatPrice } from '/functions/vat.js'
+import { ApiError, jsonFetch } from '/functions/api.js'
+import { redirect } from '/functions/url.js'
+import { flash } from '/elements/Alert.js'
+import scriptjs from 'scriptjs'
+import { Stack } from '/components/Layout.jsx'
+import { Checkbox, Field } from '/components/Form.jsx'
+import { CountrySelect } from '/components/CountrySelect.jsx'
+import { importScript } from '/functions/script.js'
+import { isAuthenticated } from '/functions/auth.js'
 
-export function PremiumButton({ children, plan, price, duration }) {
-  const [payment, togglePayment] = useToggle(false);
-  const description = `Compte premium ${duration} mois`;
+export function PremiumButton ({ children, plan, price, duration, stripeKey, paypalId }) {
+  const [payment, togglePayment] = useToggle(false)
+  const description = `Compte premium ${duration} mois`
 
   if (!isAuthenticated()) {
     return (
-      <a href="/connexion?redirect=/premium" class="btn-primary btn-block">
+      <a href='/connexion?redirect=/premium' class='btn-primary btn-block'>
         Devenir premium
       </a>
-    );
+    )
   }
 
   if (payment === false) {
-    return <PrimaryButton onClick={togglePayment}>{children}</PrimaryButton>;
+    return <PrimaryButton onClick={togglePayment}>{children}</PrimaryButton>
   }
 
   return (
     <SlideIn show={true}>
-      <PaymentMethods plan={plan} price={price} description={description} />
+      <PaymentMethods plan={plan} price={price} description={description} stripeKey={stripeKey} paypalId={paypalId} />
     </SlideIn>
-  );
+  )
 }
 
-const PAYMENT_CARD = "CARD";
-const PAYMENT_PAYPAL = "PAYPAL";
+const PAYMENT_CARD = 'CARD'
+const PAYMENT_PAYPAL = 'PAYPAL'
 
-function PaymentMethods({ plan, onPaypalApproval, description, price }) {
-  const [method, setMethod] = useState(PAYMENT_CARD);
+function PaymentMethods ({ plan, onPaypalApproval, description, price, stripeKey, paypalId }) {
+  const [method, setMethod] = useState(PAYMENT_CARD)
 
   return (
-    <div class="text-left">
-      <div className="form-group mb2">
+    <div class='text-left'>
+      <div className='form-group mb2'>
         <label>Méthode de paiement</label>
-        <div class="btn-group">
+        <div class='btn-group'>
           <button
             onClick={() => setMethod(PAYMENT_CARD)}
-            class={classNames("btn-secondary btn-small", method === PAYMENT_CARD && "active")}
+            class={classNames('btn-secondary btn-small', method === PAYMENT_CARD && 'active')}
           >
-            <img src="/images/payment-methods.png" width="76" class="mr1" />
+            <img src='/images/payment-methods.png' width='76' class='mr1' />
           </button>
           <button
             onClick={() => setMethod(PAYMENT_PAYPAL)}
-            class={classNames("btn-secondary btn-small", method === PAYMENT_PAYPAL && "active")}
+            class={classNames('btn-secondary btn-small', method === PAYMENT_PAYPAL && 'active')}
           >
-            <img src="/images/paypal.svg" width="20" class="mr1" />
+            <img src='/images/paypal.svg' width='20' class='mr1' />
           </button>
         </div>
       </div>
       {method === PAYMENT_PAYPAL ? (
-        <PaymentPaypal planId={plan} price={price} description={description} onApprove={onPaypalApproval} />
+        <PaymentPaypal
+          planId={plan}
+          price={price}
+          description={description}
+          onApprove={onPaypalApproval}
+          paypalId={paypalId}
+        />
       ) : (
-        <PaymentCard plan={plan} />
+        <PaymentCard plan={plan} publicKey={stripeKey} />
       )}
     </div>
-  );
+  )
 }
 
-function PaymentPaypal({ planId, price, description }) {
-  const container = useRef(null);
-  const approveRef = useRef(null);
-  const currency = "EUR";
-  const [country, setCountry] = useState(null);
-  const [loading, toggleLoading] = useToggle(false);
-  const vat = country ? vatPrice(price, country) : null;
+function PaymentPaypal ({ planId, price, description, paypalId }) {
+  const container = useRef(null)
+  const approveRef = useRef(null)
+  const currency = 'EUR'
+  const [country, setCountry] = useState(null)
+  const [loading, toggleLoading] = useToggle(false)
+  const vat = country ? vatPrice(price, country) : null
 
-  approveRef.current = async (orderId) => {
-    toggleLoading();
+  approveRef.current = async orderId => {
+    toggleLoading()
     try {
-      await jsonFetch(`/api/premium/paypal/${orderId}`, { method: "POST" });
-      await redirect("?success=1");
+      await jsonFetch(`/api/premium/paypal/${orderId}`, { method: 'POST' })
+      await redirect('?success=1')
     } catch (e) {
       if (e instanceof ApiError) {
-        flash(e.name, "danger", null);
+        flash(e.name, 'danger', null)
       }
     }
-    toggleLoading();
-  };
+    toggleLoading()
+  }
 
   useEffect(() => {
     if (vat === null) {
-      return;
+      return
     }
-    toggleLoading();
-    const priceWithoutTax = price - vat;
+    toggleLoading()
+    const priceWithoutTax = price - vat
     scriptjs(
-      `https://www.paypal.com/sdk/js?client-id=AVMID7UVEvfkxhAWbf_xKweK5tMQL66c-6OVtaFGnY_oU4CWtuYZkmLOck13vl2sDuebyJ6KJhznBXpY&disable-funding=card,credit&integration-date=2020-12-10&currency=${currency}`,
+      `https://www.paypal.com/sdk/js?client-id=${paypalId}&disable-funding=card,credit&integration-date=2020-12-10&currency=${currency}`,
       () => {
-        toggleLoading();
-        container.current.innerHTML = "";
+        toggleLoading()
+        container.current.innerHTML = ''
         window.paypal
           .Buttons({
             style: {
-              label: "pay",
+              label: 'pay'
             },
             createOrder: (data, actions) => {
               return actions.order.create({
@@ -117,17 +123,17 @@ function PaymentPaypal({ planId, price, description }) {
                     items: [
                       {
                         name: description,
-                        quantity: "1",
+                        quantity: '1',
                         unit_amount: {
                           value: priceWithoutTax,
-                          currency_code: currency,
+                          currency_code: currency
                         },
                         tax: {
                           value: vat,
-                          currency_code: currency,
+                          currency_code: currency
                         },
-                        category: "DIGITAL_GOODS",
-                      },
+                        category: 'DIGITAL_GOODS'
+                      }
                     ],
                     amount: {
                       currency_code: currency,
@@ -135,78 +141,78 @@ function PaymentPaypal({ planId, price, description }) {
                       breakdown: {
                         item_total: {
                           currency_code: currency,
-                          value: priceWithoutTax,
+                          value: priceWithoutTax
                         },
                         tax_total: {
                           currency_code: currency,
-                          value: vat,
-                        },
-                      },
-                    },
-                  },
-                ],
-              });
+                          value: vat
+                        }
+                      }
+                    }
+                  }
+                ]
+              })
             },
-            onApprove: (data) => {
-              approveRef.current(data.orderID);
-            },
+            onApprove: data => {
+              approveRef.current(data.orderID)
+            }
           })
-          .render(container.current);
+          .render(container.current)
         return () => {
-          container.current.innerHTML = "";
-        };
+          container.current.innerHTML = ''
+        }
       }
-    );
-  }, [description, planId, price, vat]);
+    )
+  }, [description, planId, price, vat])
 
   return (
     <Stack>
       <Field
-        name="countryCode"
+        name='countryCode'
         required
         component={CountrySelect}
         value={country}
-        onChange={(e) => setCountry(e.target.value)}
+        onChange={e => setCountry(e.target.value)}
       >
         Pays de résidence
       </Field>
-      {country && <div style={{ minHeight: 52, display: loading ? "none" : null }} ref={container} />}
+      {country && <div style={{ minHeight: 52, display: loading ? 'none' : null }} ref={container} />}
       {loading && (
-        <Button class="btn-primary btn-block" loading>
+        <Button class='btn-primary btn-block' loading>
           Chargement...
         </Button>
       )}
     </Stack>
-  );
+  )
 }
 
-function PaymentCard({ plan }) {
-  const [subscription, toggleSubscription] = useToggle(true);
-  const [loading, toggleLoading] = useToggle(false);
+function PaymentCard ({ plan, publicKey }) {
+  const [subscription, toggleSubscription] = useToggle(true)
+  const [loading, toggleLoading] = useToggle(false)
   const startPayment = async () => {
-    toggleLoading();
+    toggleLoading()
     try {
-      const Stripe = await importScript("https://js.stripe.com/v3/", "Stripe");
-      const stripe = new Stripe("pk_test_4xz0aH0LjCHk6XQOsKJy1qZh");
-      const { id } = await jsonFetch(`/api/premium/${plan}/stripe/checkout?subscription=${subscription ? "1" : "0"}`, {
-        method: "POST",
-      });
-      stripe.redirectToCheckout({ sessionId: id });
+      const Stripe = await importScript('https://js.stripe.com/v3/', 'Stripe')
+      const stripe = new Stripe(publicKey)
+      const { id } = await jsonFetch(`/api/premium/${plan}/stripe/checkout?subscription=${subscription ? '1' : '0'}`, {
+        method: 'POST'
+      })
+      stripe.redirectToCheckout({ sessionId: id })
     } catch (e) {
-      flash(e instanceof ApiError ? e.name : e, "error");
-      toggleLoading();
+      flash(e instanceof ApiError ? e.name : e, 'error')
+      toggleLoading()
     }
-  };
+  }
 
   return (
     <Stack gap={2}>
-      <Checkbox id="subscription" name="subscription" checked={subscription} onChange={toggleSubscription}>
+      <Checkbox id='subscription' name='subscription' checked={subscription} onChange={toggleSubscription}>
         Renouveller automatiquement
       </Checkbox>
-      <PrimaryButton size="block" onClick={startPayment} loading={loading}>
-        {subscription ? "S'abonner via" : "Payer via "}
-        <img src="/images/stripe.svg" height="20" style={{ marginBottom: "-2px", marginLeft: ".4rem" }} />
+      <PrimaryButton size='block' onClick={startPayment} loading={loading}>
+        {subscription ? "S'abonner via" : 'Payer via '}
+        <img src='/images/stripe.svg' height='20' style={{ marginBottom: '-2px', marginLeft: '.4rem' }} />
       </PrimaryButton>
     </Stack>
-  );
+  )
 }
---
+++ src/Infrastructure/Payment/PaymentTwigExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Infrastructure\Payment;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * Ajoute des méthodes permettant de récupérer des infos publics pour les paiements
+ */
+class PaymentTwigExtension extends AbstractExtension
+{
+
+    private string $stripePublicKey;
+    private string $paypalClientId;
+
+    public function __construct(string $stripePublicKey = '', string $paypalClientId = '')
+    {
+        $this->stripePublicKey = $stripePublicKey;
+        $this->paypalClientId = $paypalClientId;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('stripeKey', [$this, 'getStripePublicKey']),
+            new TwigFunction('paypalClientId', [$this, 'getPaypalClientId']),
+        ];
+    }
+
+    public function getStripePublicKey(): string
+    {
+        return $this->stripePublicKey;
+    }
+
+    public function getPaypalClientId(): string
+    {
+        return $this->paypalClientId;
+    }
+}
