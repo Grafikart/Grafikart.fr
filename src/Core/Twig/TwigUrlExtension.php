d48cde7d571cdc032f55cc9f8e2bd534a724bade
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-09 20:35:39 +0100

    fix: autolink

--- src/Core/Twig/TwigUrlExtension.php
+++ src/Core/Twig/TwigUrlExtension.php
@@ -89,15 +89,15 @@ class TwigUrlExtension extends AbstractExtension
 
     public function autoLink(string $string): string
     {
-        $regexp = '/(<a.*?>)?(https?)?(:\/\/)?(\w+\.)?(\w+)\.([\w\/\-_.~&=?]+)(<\/a>)?/i';
-        $anchor = '<a href="%s://%s" target="_blank">%s</a>';
+        $regexp = '/(<a.*?>)?(https?:)?(\/\/)(\w+\.)?(\w+\.[\w\/\-_.~&=?]+)(<\/a>)?/i';
+        $anchor = '<a href="%s//%s" target="_blank" rel="noopener noreferrer">%s</a>';
 
         preg_match_all($regexp, $string, $matches, PREG_SET_ORDER);
 
         foreach ($matches as $match) {
-            if (empty($match[1]) && empty($match[7])) {
-                $protocol = $match[2] ? $match[2] : 'http';
-                $replace = sprintf($anchor, $protocol, $match[0], $match[0]);
+            if (empty($match[1]) && empty($match[6])) {
+                $protocol = $match[2] ? $match[2] : 'https:';
+                $replace = sprintf($anchor, $protocol, $match[5], $match[0]);
                 $string = str_replace($match[0], $replace, $string);
             }
         }
---
+++ tests/Core/Twig/TwigUrlExtensionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\Core\Twig;
+
+use ApiPlatform\Core\Api\UrlGeneratorInterface;
+use App\Core\Twig\TwigUrlExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Serializer\SerializerInterface;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
+
+class TwigUrlExtensionTest extends TestCase
+{
+
+    private TwigUrlExtension $extension;
+
+    public function setUp(): void
+    {
+        $serializer = $this->createMock(SerializerInterface::class);
+        $uploaderHelper = $this->createMock(UploaderHelper::class);
+        $urlGenerator = $this->createMock(UrlGeneratorInterface::class);
+        $this->extension = new TwigUrlExtension($urlGenerator, $uploaderHelper, $serializer);
+    }
+
+    public function testAutolink(): void
+    {
+        $result = $this->extension->autoLink(<<<HTML
+            Découverte du jour, quand vous mettez le lien d'une MR en cours en commentaire dans votre code. Github vous prévien… https://t.co/KFais4txqG
+        HTML);
+        $this->assertEquals(<<<HTML
+            Découverte du jour, quand vous mettez le lien d'une MR en cours en commentaire dans votre code. Github vous prévien… <a href="https://t.co/KFais4txqG" target="_blank" rel="noopener noreferrer">https://t.co/KFais4txqG</a>
+        HTML, $result);
+    }
+}
