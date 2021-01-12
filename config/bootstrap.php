23b1d837d1f96852ec91a52e5db1a97e8b116501
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-12 20:30:55 +0100

    fix: langue des dates

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,8 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+setlocale(LC_TIME, 'fr_FR');
+
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
