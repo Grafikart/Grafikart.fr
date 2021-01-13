f3cfc76b1f84fbc0dd5737d72cef47fc9ca76edb
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-13 21:47:53 +0100

    fix: déblocage badge

--- src/Http/Api/Controller/BadgeController.php
+++ src/Http/Api/Controller/BadgeController.php
@@ -4,14 +4,14 @@ namespace App\Http\Api\Controller;
 
 use App\Domain\Badge\BadgeService;
 use App\Domain\Badge\Entity\Badge;
-use App\Http\Admin\Controller\BaseController;
+use App\Http\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class BadgeController extends BaseController
+class BadgeController extends AbstractController
 {
     private BadgeService $service;
 
@@ -27,12 +27,12 @@ class BadgeController extends BaseController
     public function unlock(string $badgeName): JsonResponse
     {
         if (!in_array($badgeName, Badge::REQUEST_UNLOCKABLE)) {
-            return new JsonResponse([], Response::HTTP_FORBIDDEN);
+            throw new AccessDeniedException('Aucun badge ne correspond à ce nom');
         }
 
-        $unlocks = $this->service->unlock($this->getUser(), $badgeName);
+        $unlocks = $this->service->unlock($this->getUserOrThrow(), $badgeName);
         if (null === $unlocks || empty($unlocks)) {
-            return new JsonResponse([], Response::HTTP_UNPROCESSABLE_ENTITY);
+            return new JsonResponse(null, Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
         return $this->json($unlocks[0]->getBadge());
