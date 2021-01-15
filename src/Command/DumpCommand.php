0a1bfa465fd0b1d7c600d6587a8967dc376f9832
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-15 10:51:11 +0100

    feat: dump base de données

---
+++ src/Command/DumpCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use League\Flysystem\FilesystemInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+
+class DumpCommand extends Command
+{
+
+    protected static $defaultName = 'app:dump';
+    private EntityManagerInterface $em;
+    private string $dumpPath;
+    private FilesystemInterface $filesystem;
+
+    public function __construct(EntityManagerInterface $em, string $projectPath, FilesystemInterface $filesystem)
+    {
+        parent::__construct();
+        $this->em = $em;
+        $this->dumpPath = $projectPath;
+        $this->filesystem = $filesystem;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->writeln('Export de la base de données');
+        $dumpFile = $this->dumpPath . '/dump.tar';
+
+        // On génère le dump SQL
+        $params = $this->em->getConnection()->getParams();
+        $process = new Process(['pg_dump', '-U', $params['user'], '-Ft', '-h', $params['host'], '-f', $dumpFile]);
+        $process->setEnv(['PGPASSWORD' => $params['password']]);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $io->error("Impossible d'exporter la base de données");
+            $io->error($process->getErrorOutput());
+            return 1;
+        }
+
+        // On sauvegarde le fichier dans notre backup storage
+        $stream = fopen($dumpFile, 'r');
+        if ($stream === false) {
+            throw new \Exception("Impossible de lire le fichier \"$dumpFile\"");
+        }
+        $date = date('Y-m-d');
+        try {
+            $this->filesystem->writeStream("grafikart-{$date}.tar", $stream);
+        } catch (\Exception $e) {
+            unlink($dumpFile);
+            throw $e;
+        }
+        unlink($dumpFile);
+        $io->success('La base de donnée a bien été sauvegardée');
+
+        return 0;
+    }
+}
---
+++ src/Infrastructure/Storage/DropboxFilesystemFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Infrastructure\Storage;
+
+use League\Flysystem\Filesystem;
+use Spatie\Dropbox\Client;
+use Spatie\FlysystemDropbox\DropboxAdapter;
+
+class DropboxFilesystemFactory
+{
+
+    private string $accessToken;
+
+    public function __construct(string $accessToken)
+    {
+        $this->accessToken = $accessToken;
+    }
+
+    public function createFilesystem(): Filesystem
+    {
+        $client = new Client($this->accessToken);
+        $adapter = new DropboxAdapter($client);
+        return new Filesystem($adapter, ['case_sensitive' => false]);
+    }
+}
