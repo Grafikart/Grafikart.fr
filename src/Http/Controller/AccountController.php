63798e6d39d3dc584a3b552b7ea01672ce4ddd39
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-18 23:55:21 +0100

    fix: message confirmation changement email

--- src/Http/Controller/AccountController.php
+++ src/Http/Controller/AccountController.php
@@ -101,7 +101,7 @@ class AccountController extends AbstractController
                 $this->em->flush();
                 if ($user->getEmail() !== $data->email) {
                     $this->addFlash(
-                        'success',
+                        'info',
                         "Votre profil a bien été mis à jour, un email a été envoyé à {$data->email} pour confirmer votre changement"
                     );
                 } else {
