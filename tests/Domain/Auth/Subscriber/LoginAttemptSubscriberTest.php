63228a1ed80e60e1d6a11859d70c786396541e18
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-23 10:01:31 +0100

    fix: style forum message
    Fix #284

--- tests/Domain/Auth/Subscriber/LoginAttemptSubscriberTest.php
+++ tests/Domain/Auth/Subscriber/LoginAttemptSubscriberTest.php
@@ -7,6 +7,7 @@ use App\Domain\Auth\Service\LoginAttemptService;
 use App\Domain\Auth\Subscriber\LoginSubscriber;
 use App\Domain\Auth\User;
 use App\Tests\EventSubscriberTest;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 
 class LoginAttemptSubscriberTest extends EventSubscriberTest
@@ -32,11 +33,10 @@ class LoginAttemptSubscriberTest extends EventSubscriberTest
     private function getSubscriber(): LoginSubscriber
     {
         /* @var MockObject|LoginAttemptService $service */
-        $this->service = $this->getMockBuilder(LoginAttemptService::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->service = $this->createMock(LoginAttemptService::class);
+        $em = $this->createMock(EntityManagerInterface::class);
 
-        return new LoginSubscriber($this->service);
+        return new LoginSubscriber($this->service, $em);
     }
 
     private function getEvent(): BadPasswordLoginEvent
