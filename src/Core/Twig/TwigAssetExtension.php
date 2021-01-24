945d5c6484b0a5d2fa8fe9f2106219ae5ad73f03
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-01-24 10:52:31 +0100

    feat: rollup build
    Rollback vers rollup pour le build des assets.

--- src/Core/Twig/TwigAssetExtension.php
+++ src/Core/Twig/TwigAssetExtension.php
@@ -78,7 +78,7 @@ class TwigAssetExtension extends AbstractExtension
             return $request ? "http://{$request->getHost()}:3000/{$name}" : '';
         }
 
-        $name = $this->getAssetPaths()[$name]['file'] ?? '';
+        $name = $this->getAssetPaths()[$name] ?? '';
 
         return "/assets/$name";
     }
