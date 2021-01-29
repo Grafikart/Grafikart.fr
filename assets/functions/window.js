bebc4fef64edd39db6358418471381b23decdcfa
jonathan <contact@grafikart.fr>
2021-01-29 20:04:08 +0100

    fix: support de brave
    Brave en mode agressif bloque le localStorage

--- assets/functions/window.js
+++ assets/functions/window.js
@@ -8,13 +8,20 @@ export function windowHeight () {
 }
 
 const uuid = new Date().getTime().toString()
-localStorage.setItem('windowId', uuid)
-window.addEventListener('focus', function () {
+if (localStorage) {
   localStorage.setItem('windowId', uuid)
-})
+  window.addEventListener('focus', function () {
+    localStorage.setItem('windowId', uuid)
+  })
+}
+
 /**
  * Renvoie true si la fenêtre est active ou si elle a été la dernière fenêtre active
  */
 export function isActiveWindow () {
-  return uuid === localStorage.getItem('windowId')
+  if (localStorage) {
+    return uuid === localStorage.getItem('windowId')
+  } else {
+    return true
+  }
 }
