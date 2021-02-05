7d7508caee4af8119dd813ad5d1d9c84ecaddc7d
Jonathan <395137+Grafikart@users.noreply.github.com>
2021-02-05 14:10:26 +0100

    feat: sticky header

--- assets/functions/timers.js
+++ assets/functions/timers.js
@@ -14,6 +14,30 @@ export function debounce (func, wait, immediate) {
 }
 
 /**
+ * Throttle un callback
+ */
+export function throttle(callback, delay) {
+  let last;
+  let timer;
+  return function () {
+    let context = this;
+    let now = +new Date();
+    let args = arguments;
+    if (last && now < last + delay) {
+      // le délai n'est pas écoulé on reset le timer
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        last = now;
+        callback.apply(context, args);
+      }, delay);
+    } else {
+      last = now;
+      callback.apply(context, args);
+    }
+  };
+}
+
+/**
  * Version asynchrone du timeout
  *
  * @param {number} duration
