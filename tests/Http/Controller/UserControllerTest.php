3e87d989e0e39c955cb6f8231c0482bc4f93af9e
Jonathan Boyer <boyer.jonathan@gmail.com>
2021-02-19 22:18:40 +0100

    test: ninja !

--- tests/Http/Controller/UserControllerTest.php
+++ tests/Http/Controller/UserControllerTest.php
@@ -23,4 +23,21 @@ class UserControllerTest extends WebTestCase
         $this->client->request('GET', '/profil/azijeazejoaz');
         $this->assertResponseStatusCodeSame(404);
     }
+
+    public function testSwitchUser(): void
+    {
+        /** @var User $user1 */
+        /** @var User $admin */
+        ['user1' => $user1, 'user_admin' => $admin] = $this->loadFixtures(['users']);
+        // Un utilisateur normal ne peut pas devenir admin
+        $this->login($user1);
+        $this->client->request('GET', "/?_ninja={$admin->getUsername()}");
+        $this->assertResponseStatusCodeSame(403);
+
+        // Un admin peut devenir user
+        $this->login($admin);
+        $this->client->request('GET', "/?_ninja={$user1->getUsername()}");
+        $this->assertResponseStatusCodeSame(302);
+        $this->assertResponseRedirects();
+    }
 }
