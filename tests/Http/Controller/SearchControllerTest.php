15db4383c32feef62b3307e7d454df4ab764ef3f
Jonathan <contact@grafikart.fr>
2021-04-11 19:39:42 +0200

    fix: redirection ancienne recherche

--- tests/Http/Controller/SearchControllerTest.php
+++ tests/Http/Controller/SearchControllerTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Http\Controller;
 use App\Domain\Course\Entity\Technology;
 use App\Tests\FixturesTrait;
 use App\Tests\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class SearchControllerTest extends WebTestCase
 {
@@ -28,5 +29,11 @@ class SearchControllerTest extends WebTestCase
         $this->client->request('GET', '/recherche?q='.strtolower($technology->getName()));
         $this->assertResponseStatusCodeSame(302);
         $this->assertResponseRedirects('/tutoriels/'.$technology->getSlug());
+    }
+
+    public function testRedirectLegacySearch(): void
+    {
+        $this->client->request('GET', '/search?q=demophp');
+        $this->assertResponseRedirects($this->client->getRequest()->getSchemeAndHttpHost() . '/recherche?q=demophp', Response::HTTP_PERMANENTLY_REDIRECT);
     }
 }
