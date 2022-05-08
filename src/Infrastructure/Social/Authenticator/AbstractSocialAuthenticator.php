b8243c1bb76e1c8a4b9ac49fb39b73732f5a028c
Jonathan <contact@grafikart.fr>
2022-05-08 14:18:23 +0200

    fix: remember me social auth

--- src/Infrastructure/Social/Authenticator/AbstractSocialAuthenticator.php
+++ src/Infrastructure/Social/Authenticator/AbstractSocialAuthenticator.php
@@ -21,6 +21,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
@@ -91,7 +92,10 @@ abstract class AbstractSocialAuthenticator extends OAuth2Authenticator
         $userLoader = fn () => $user;
 
         return new SelfValidatingPassport(
-            new UserBadge($user->getUserIdentifier(), $userLoader)
+            new UserBadge($user->getUserIdentifier(), $userLoader),
+            [
+                new RememberMeBadge()
+            ]
         );
     }
 
