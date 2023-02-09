0bcc5478c6a8ffa31cb842add0547ee1e22d9cae
Jonathan <contact@grafikart.fr>
2023-02-09 10:05:37 +0100

    fix: cache interface

--- src/Http/Admin/Controller/CacheController.php
+++ src/Http/Admin/Controller/CacheController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Admin\Controller;
 
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class CacheController extends BaseController
 {
     #[Route(path: '/cache/clean', name: 'cache_clean', methods: ['POST'])]
-    public function clean(AdapterInterface $cache): RedirectResponse
+    public function clean(CacheInterface $cache): RedirectResponse
     {
         $this->addFlash('success', 'Le cache a bien été supprimé');
         $cache->clear();
