7629d9c4341969918f7f745fa5e9c5ecc61307c6
Jonathan <contact@grafikart.fr>
2023-02-09 11:51:34 +0100

    fix: nettoyage cache controller

--- src/Http/Admin/Controller/CacheController.php
+++ src/Http/Admin/Controller/CacheController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Admin\Controller;
 
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 
 class CacheController extends BaseController
 {
     #[Route(path: '/cache/clean', name: 'cache_clean', methods: ['POST'])]
-    public function clean(CacheInterface $cache): RedirectResponse
+    public function clean(CacheItemPoolInterface $cache): RedirectResponse
     {
         $this->addFlash('success', 'Le cache a bien été supprimé');
         $cache->clear();
