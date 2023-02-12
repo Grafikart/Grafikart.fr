f7399820655ca07c5461eaf91bbc3b52f7d0c87b
Jonathan <contact@grafikart.fr>
2023-02-12 17:48:18 +0100

    rollback: cache cleaning from dashboard

--- src/Http/Admin/Controller/CacheController.php
+++ src/Http/Admin/Controller/CacheController.php
@@ -2,21 +2,17 @@
 
 namespace App\Http\Admin\Controller;
 
-use App\Http\Twig\TwigAssetExtension;
 use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 
 class CacheController extends BaseController
 {
     #[Route(path: '/cache/clean', name: 'cache_clean', methods: ['POST'])]
-    public function clean(CacheItemPoolInterface $cache, CacheInterface $sfCache): RedirectResponse
+    public function clean(CacheItemPoolInterface $cache): RedirectResponse
     {
         $this->addFlash('success', 'Le cache a bien été supprimé');
         $cache->clear();
-        // Nettoie le cache des assets
-        $sfCache->delete(TwigAssetExtension::CACHE_KEY);
 
         return $this->redirectToRoute('admin_home');
     }
