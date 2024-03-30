9c75eb24f9fb174d17c76d56706bc118d3d4206b
Jonathan <contact@grafikart.fr>
2024-03-30 13:02:47 +0100

    bump: deps

--- rector.php
+++ rector.php
@@ -1,23 +1,10 @@
 <?php
-
-declare(strict_types=1);
-
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SensiolabsSetList;
-use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests'
-    ]);
+return RectorConfig::configure()
+    ->withRules([
 
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-        SensiolabsSetList::FRAMEWORK_EXTRA_61,
+    ])->withSets([
+        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_82,
+        \Rector\Symfony\Set\SymfonySetList::SYMFONY_64,
     ]);
-};
