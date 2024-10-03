95e4a67f40263b1af966920da047eea4b27a8f93
Jonathan <contact@grafikart.fr>
2024-10-03 18:38:17 +0200

    ref: vitest

--- tests/js/functions/size.test.js
+++ tests/js/functions/size.test.js
@@ -1,10 +1,11 @@
-import { human } from '/functions/size'
+import { test, expect } from "vitest";
+import { human } from "/functions/size";
 
-test('Convertit les kilos octets', () => {
-  expect(human(1024)).toBe('1k')
-  expect(human(2048)).toBe('2k')
-  expect(human(1536)).toBe('1.5k')
-  expect(human(343361)).toBe('336k')
-  expect(human(4911)).toBe('4.8k')
-  expect(human(335421910)).toBe('320M')
-})
+test("Convertit les kilos octets", () => {
+  expect(human(1024)).toBe("1k");
+  expect(human(2048)).toBe("2k");
+  expect(human(1536)).toBe("1.5k");
+  expect(human(343361)).toBe("336k");
+  expect(human(4911)).toBe("4.8k");
+  expect(human(335421910)).toBe("320M");
+});
