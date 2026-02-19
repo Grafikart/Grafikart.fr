1ce1b9d81ef0cf2383b0d117912d9f1abfcbb370
Jonathan <contact@grafikart.fr>
2026-02-19 18:43:03 +0100

    fix(front): improve editor styling

--- resources/views/cms.blade.php
+++ resources/views/cms.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class($appearance)>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class([$appearance, 'cms'])>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
