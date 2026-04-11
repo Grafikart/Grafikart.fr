435b98237b63818ec219872938cbbb877c37facd
jonathan <contact@grafikart.fr>
2026-04-11 14:31:15 +0200

    fix: missing attachment guard on home

--- resources/views/components/molecules/home-post-card.blade.php
+++ resources/views/components/molecules/home-post-card.blade.php
@@ -1,11 +1,13 @@
 @props(['index', 'post'])
 
 <article @class(["items-start flex relative gap-5 border-b pb-4 lg:pb-8 last:border-none last:pb-0", "lg:flex-col lg:border-none lg:pb-0!" => $index < 2, "lg:flex-row-reverse" => $index > 1])>
+    @if($post->attachment)
     <img @class(['size-22 rounded-sm', 'lg:hidden' => $index < 2]) src="{{ $post->attachment->url(160, 160) }}" width="160" height="160" alt="">
-    @if($index === 0)
+    @endif
+    @if($index === 0 && $post->attachment)
         <img class="rounded-sm hidden lg:block" src="{{ $post->attachment->url(370, 205) }}" alt="" width="370" height="205">
     @endif
-    @if($index === 1)
+    @if($index === 1 && $post->attachment)
         <img class="rounded-sm hidden lg:block object-cover" src="{{ $post->attachment->url(360, 450) }}" alt="" width="370" height="550">
     @endif
     <div class="flex-1">
