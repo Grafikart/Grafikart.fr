2b2b4e5a648e62c4edf7f22ba2a6e21e262537a1
Jonathan <contact@grafikart.fr>
2026-04-28 21:34:47 +0200

    feat: freelance link in contact

--- resources/views/components/atoms/alert.blade.php
+++ resources/views/components/atoms/alert.blade.php
@@ -1,11 +1,13 @@
-@props(['type'])
+@props(['type', 'size' => 'default'])
 
 <x-atoms.card {{ $attributes->class([
-'p-4 flex items-center border-l-3 gap-4',
-'border-l-primary' => $type === 'info'
+'flex items-center border-l-3',
+'border-l-primary' => $type === 'info',
+'p-4 gap-4' => $size === 'default',
+'p-3 gap-2 text-sm' => $size === 'sm'
 ])}}>
     @if($type === 'info')
-    <x-lucide-info class="text-primary size-5 flex-none"/>
+    <x-lucide-info @class(["text-primary flex-none", "size-5" => $size === 'default', 'size-4' => $size === 'sm'])/>
     @endif
     <p class="prose">
         {{ $slot  }}
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,25 +7,34 @@
     <div class="container pb-10 bg-background-light">
         <h1 class="text-page-title mb-4">Me contacter</h1>
         <p class="text-lg text-muted max-w-175">
-            Si vous avez des problèmes ou un bug dans votre code n'utilisez pas ce formulaire, utilisez plutôt le
-            système de support présent sur les vidéos.
+            Vous avez un problème avec l'utilisation du site ou avec votre abonnement ? Vous pouvez utiliser ce
+            formulaire pour me contacter.
         </p>
     </div>
 
     <div class="border-t bg-background py-10">
         <div class="container">
             <div class="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-10">
-                <form class="grid grid-cols-2 gap-4" method="post" action="{{ route('contact') }}">
-                    <x-molecules.field name="name" required label="Votre nom"/>
-                    <x-molecules.field name="email" required type="email" label="Votre email"/>
-                    <x-molecules.field name="content" required label="Votre message" type="textarea"
-                                       class="col-span-2"/>
-                    <x-atoms.button>
-                        <x-lucide-mail/>
-                        Envoyer
-                    </x-atoms.button>
-                </form>
-                <div class="space-y-5">
+                <div>
+                    <x-atoms.alert type="info" size="sm" class="mb-6">
+                        Si vous avez des problèmes dans votre code n'utilisez utilisez
+                        plutôt le système de support présent sous la vidéo.
+                    </x-atoms.alert>
+                    <form class="grid grid-cols-2 gap-4" method="post" action="{{ route('contact') }}">
+                        <x-molecules.field name="name" required label="Votre nom"/>
+                        <x-molecules.field name="email" required type="email" label="Votre email"/>
+                        <x-molecules.field name="content" required label="Votre message" type="textarea"
+                                           class="col-span-2"/>
+                        <x-atoms.button>
+                            <x-lucide-mail/>
+                            Envoyer
+                        </x-atoms.button>
+                        <p class="text-muted col-span-2 text-sm">
+                            J'essaie de traiter les emails le plus rapidement possible, vous devriez recevoir une réponse dans les 5 jours.
+                        </p>
+                    </form>
+                </div>
+                <div class="space-y-10">
                     <div>
                         <h2 class="text-xl text-foreground-title font-bold mb-4">Sur les réseaux sociaux</h2>
                         @php
@@ -48,6 +57,18 @@
                         <p class="prose text-muted">
                             Vous avez envie de parler en direct avec d'autres membres du site ? N'hésitez pas à faire un
                             tour sur le <a href="/tchat">serveur discord #grafikart</a>
+                        </p>
+                    </div>
+                    <div>
+                        <h2 class="text-xl text-foreground-title font-bold mb-4">Besoin d'un freelance ?</h2>
+                        <p class="prose text-muted">
+                            Si vous souhaitez faire appel à moi pour une mission freelance, vous pouvez passer par <a
+                                target="_blank"
+                                href="https://jonathan-boyer.fr/">mon portfolio</a> ou via
+                            <a target="_blank" href="https://www.malt.fr/profile/jonathanboyer3"
+                               class="inline-flex items-center gap-1">
+                                mon profil malt
+                            </a>
                         </p>
                     </div>
                 </div>
