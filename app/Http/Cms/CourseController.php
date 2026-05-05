e55d46db95fdf5349714cd3069663ab759284ba5
jonathan <contact@grafikart.fr>
2026-05-05 22:20:22 +0200

    fix: cloning for courses

--- app/Http/Cms/CourseController.php
+++ app/Http/Cms/CourseController.php
@@ -58,10 +58,13 @@ class CourseController extends CmsController
         // Create a clone to copy a course
         if ($clone) {
             $course = clone Course::findOrFail($clone);
-            $course->setAttribute('id', null);
             $course->load('technologies');
+            $course->created_at = $course->created_at->addDay();
+            $course->source = null;
+            $course->youtube_id = null;
+            $course->setAttribute('id', null);
 
-            return $this->cmsCreate(['index' => CourseFormData::from($course)]);
+            return $this->cmsCreate(['item' => CourseFormData::from($course)]);
         }
 
         return $this->cmsCreate();
